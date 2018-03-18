62e4434d8829dda95c41c34d738bc9db56b4b1a0
Ian Lewis <ian.lewis748@gmail.com>
2018-03-18 11:59:10 -0400

    added google tag manager

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -6,6 +6,7 @@ import Hero from './hero';
 import StationInput from './station_search/station_input';
 import ArrivalsContainer from './arrivals_table/arrivals_container';
 import FullPageSpinner from './loading_spinners/full_page_spinner';
+import googleTagManager from '../functions/google-tag-manager';
 import {
   intitializeFirebase,
   updateTimestamp,
@@ -16,12 +17,15 @@ import {
 const history = createHistory();
 
 class Main extends Component {
+
   componentWillMount() {
     this.props.dispatch(intitializeFirebase());
     this.props.dispatch(setInitialArrivals());
     this.props.dispatch(updateTimestamp());
     this.props.dispatch(setTimestampInterval());
+    googleTagManager();
   }
+
   render() {
     return (
       <div>
