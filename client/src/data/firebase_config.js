c657283900ab3d5613d96fb69ba65946f2d9a7c5
Ian Lewis <ian.lewis748@gmail.com>
2018-03-18 19:55:55 -0400

    corrected errors in firebase config vars

--- client/src/data/firebase_config.js
+++ client/src/data/firebase_config.js
@@ -8,12 +8,12 @@ const developmentConfig = {
 }
 
 const productionConfig = {
-  "apiKey": process.env.FIREBASE_KEY,
-  "authDomain": process.env.FIREBASE_DOMAIN,
-  "databaseURL": process.env.FIREBASE_URL,
-  "projectId": process.env.FIREBASE_PROJECT_ID,
-  "storageBucket": process.env.FIREBASE_STORAGE_BUCKET,
-  "messagingSenderId": process.env.FIREBASE_MESSAGING_SENDER_ID
+  "apiKey": process.env.FIREBASE_CONFIG_API_KEY,
+  "authDomain": process.env.FIREBASE_CONFIG_AUTH_DOMAIN,
+  "databaseURL": process.env.FIREBASE_CONFIG_DATABASE_URL,
+  "projectId": process.env.FIREBASE_CONFIG_PROJECT_ID,
+  "storageBucket": process.env.FIREBASE_CONFIG_STORAGE_BUCKET,
+  "messagingSenderId": process.env.FIREBASE_CONFIG_MESSAGING_SENDER_ID
 }
 
 const config = (process.env.NODE_ENV === "production") ? productionConfig : developmentConfig;
