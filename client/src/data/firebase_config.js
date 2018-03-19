f2d804f357a05b8f2d1f3e62cf1e03ec87dfbe48
Ian Lewis <ian.lewis748@gmail.com>
2018-03-18 20:45:59 -0400

    updates

--- client/src/data/firebase_config.js
+++ client/src/data/firebase_config.js
@@ -1,4 +1,4 @@
-const developmentConfig = {
+const config = {
   "apiKey": process.env.REACT_APP_FIREBASE_KEY,
   "authDomain": process.env.REACT_APP_FIREBASE_DOMAIN,
   "databaseURL": process.env.REACT_APP_FIREBASE_URL,
@@ -7,15 +7,4 @@ const developmentConfig = {
   "messagingSenderId": process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 }
 
-const productionConfig = {
-  "apiKey": process.env.FIREBASE_CONFIG_API_KEY,
-  "authDomain": process.env.FIREBASE_CONFIG_AUTH_DOMAIN,
-  "databaseURL": process.env.FIREBASE_CONFIG_DATABASE_URL,
-  "projectId": process.env.FIREBASE_CONFIG_PROJECT_ID,
-  "storageBucket": process.env.FIREBASE_CONFIG_STORAGE_BUCKET,
-  "messagingSenderId": process.env.FIREBASE_CONFIG_MESSAGING_SENDER_ID
-}
-
-const config = developmentConfig; /*(process.env.NODE_ENV === "production") ? productionConfig : developmentConfig;*/
-console.log(config);
 export default config;
